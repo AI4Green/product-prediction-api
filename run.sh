a0dceca82acd76fe04aec450b3abd472698a5d2f
Fong Mun Hong <fongmh1947@gmail.com>
2023-10-23 22:32:49 +0000

    Delete run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-export ASKCOS_REGISTRY=registry.gitlab.com/mlpds_mit/askcosv2
-docker build -f Dockerfile_gpu -t ${ASKCOS_REGISTRY}/forward_predictor/augmented_transformer:1.0-gpu .
-sh scripts/serve_gpu_in_docker.sh
